457bbafbec64d87293717c6e74e7e4174187c616
Khoa Le <binkhoa1812@gmail.com>
2025-10-08 13:45:25 +1100

    feat: initial backend structure setups

---
+++ backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  ignorePatterns: ['node_modules/', 'dist/', 'coverage/'],
+};
